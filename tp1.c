994241eef03ae4ef4244842b5719229b06fef595
David LIVET <david.livet@orange.fr>
2015-03-23 10:13:24 +0100

    Add named fifo

--- tp1.c
+++ tp1.c
@@ -5,104 +5,68 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <sys/time.h>       
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+
 #define PINGPONG_MAX 2000
-
-/*
-	Son Handler
-	Function executed when SIGUSR1 is sended
-	Before his death, son send a SIGCHLD to his father (else is not really dead) 
-*/
-void son_handler(int sig){
-	if(sig == SIGUSR1){
-		printf("Son   : --->down<---\n");
-		kill(getppid(),SIGCHLD);
-		exit(EXIT_SUCCESS);
-	}
-}
-
-/*
-	Father Handler
-	Function executed when SIGUSR1 is sended
-*/
-void father_handler(int sig){
-	if(sig == SIGUSR1){
-		printf("Father: --->hit<---\n");
-		exit(EXIT_SUCCESS);
-	}
-}
+#define FIFO_ABSOLUTE_NAME "/tmp/fifo"
 
 int main(int argc, char ** argv){
-	int data = 0, son_processus = 0;
-	sigset_t mask;
-	struct sigaction signal_conf;
-	int father_to_son_pipefd[2], son_to_father_pipefd[2];
+	int data = 0;
+	int fifo_send, fifo_receive;
+	char name_fifo1[20], name_fifo2[20];
 	
-	// SIG conf
-	sigfillset(&mask); // Set all bit mask to 1. Default all signals are ignored 
-	sigdelset(&mask,SIGUSR1); // Un-mask SIGUSR1
-	signal_conf.sa_flags = 0;
+	if(argc != 3){
+		fprintf(stderr,"usage: ./tp1 local_process remote_process\n");
+		exit(3);
+	}
+	strcpy(name_fifo1,FIFO_ABSOLUTE_NAME);
+	strcat(name_fifo1,argv[1]);
+	strcpy(name_fifo2,FIFO_ABSOLUTE_NAME);
+	strcat(name_fifo2,argv[2]);
 
-	// PIPE conf
-	if (pipe(father_to_son_pipefd) == -1) {
-       perror("father_to_son_pipefd");
-       exit(EXIT_FAILURE);
-   	}
-	if (pipe(son_to_father_pipefd) == -1) {
-       perror("son_to_father_pipefd");
-       exit(EXIT_FAILURE);
-   	}
-
-	son_processus = fork();
-	if (son_processus == -1) {
-       perror("fork");
-       exit(EXIT_FAILURE);
-   }
-
-	if(son_processus == 0){
-		close(father_to_son_pipefd[1]); // Close unused write end
-		close(son_to_father_pipefd[0]); // Close unused read end
-
-		signal_conf.sa_handler = son_handler;
-		signal_conf.sa_mask = mask;
-
-		sigaction(SIGUSR1,&signal_conf,NULL);
+	if(mkfifo(name_fifo1,S_IRUSR | S_IWUSR | S_IRGRP) != 0){
+		perror("mkfifo");
+		exit(EXIT_FAILURE);
+	}
+	
+	if(argv[1] > argv[2]){
+		fifo_receive = open(name_fifo1,O_RDONLY);
+		printf("%s created\n", name_fifo1);
+		fifo_send = open(name_fifo2,O_WRONLY);
+		printf("%s created\n", name_fifo2);
 
 		// Read first
 		while(data < PINGPONG_MAX){
-			read(father_to_son_pipefd[0], &data,sizeof(int));
+			read(fifo_receive, &data,sizeof(int));
 
 			data++;
-			write(son_to_father_pipefd[1],&data,sizeof(int));
+			write(fifo_send,&data,sizeof(int));
 		}	
 
-		close(father_to_son_pipefd[0]);
-		close(son_to_father_pipefd[1]);
-		while(1){
-			printf("Son   : I'm alive\n");
-			sleep(10);
-		}
+		close(fifo_send);
+		close(fifo_receive);
+		unlink(name_fifo1);
 	}
-	else{
+	else {
 		struct timeval before, after, delta;
 		double debit = 0.0;
 
-		close(father_to_son_pipefd[0]); // Close unused read end
-		close(son_to_father_pipefd[1]); // Close unused write end
-
-		sigdelset(&mask,SIGCHLD); // Un-mask SIGCHLD to receive son dead ack
-		signal_conf.sa_handler = father_handler;
-		signal_conf.sa_mask = mask;
-
-		sigaction(SIGUSR1,&signal_conf,NULL);
+		fifo_send = open(name_fifo1,O_RDONLY);
+		printf("%s created\n", name_fifo1);
+		fifo_receive = open(name_fifo2,O_WRONLY);
+		printf("%s created\n", name_fifo2);
 
 		// Write first
 		gettimeofday(&before, NULL);
 		while(data < PINGPONG_MAX){
 			data++;
-			write(father_to_son_pipefd[1],&data,sizeof(int));
+			write(fifo_send,&data,sizeof(int));
 
-			read(son_to_father_pipefd[0], &data,sizeof(int));
+			read(fifo_receive, &data,sizeof(int));
 		}
 		gettimeofday(&after, NULL);
 
@@ -110,22 +74,11 @@ int main(int argc, char ** argv){
 		delta.tv_sec = after.tv_sec - before.tv_sec;
 		delta.tv_usec = after.tv_usec - before.tv_usec;
 		debit = (1000.0*2.0*4.0*8.0)/delta.tv_usec;
-		printf("Father: execution time %ld.%06ld debit %f Mb\n",delta.tv_sec,delta.tv_usec,debit);
+		printf("execution time %ld.%06ld debit %f Mb\n",delta.tv_sec,delta.tv_usec,debit);
 
-		close(father_to_son_pipefd[0]);
-		close(son_to_father_pipefd[1]);
-
-		sleep(10);		
-		printf("Father: I'll kill you son\n");	
-
-		// Send SIGUSR1 to son process
-		kill(son_processus,SIGUSR1);
-		wait(NULL); // Wait SIGCHLD
-		
-		while(1){
-			printf("Father: I'm your father\n");
-			sleep(10);
-		}
+		close(fifo_send);
+		close(fifo_receive);
+		unlink(name_fifo1);
 	}
 
 	return EXIT_SUCCESS;
